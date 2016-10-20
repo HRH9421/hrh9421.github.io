606e5755ccabb4b040bd5223e5ba7f5b66e9de76
HRH9421 <hrh9421@foxmail.com>
2016-10-20 09:57:04 +0800

    修改

--- dangdang-app-angularsjs/assets/api/service.js
+++ dangdang-app-angularsjs/assets/api/service.js
@@ -6,16 +6,16 @@
 app.factory('commonService', ['$http', function($http) {
     var service = {};
     service.getData = function(type, callBack) {
-        $http({
-                url: '/dangdang-app-angularsjs/data/book_' + type + '.json',
-                method: 'get'
-            })
-            .then(function(res) {
-                console.dir(red)
-                callBack(res)
-            }, function(err) {
-                console.dir(err)
-            })
-    }
+          $http({
+                  url: '/dangdang-app-angularjs/data/book_' + type + '.json',
+                  method: 'get'
+              })
+              .then(function(res) {
+                  console.log('获取数据成功')
+                  callBack(res);
+              }, function(err) {
+                  console.dir(err);
+              })
+      }
     return service;
 }])
